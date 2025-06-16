c2845d7654e6c5665589176d4d6d00a6c37f39a7
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-06-16 08:37:11 -0600

    Cambios a la paleta de colores

--- app/src/main/java/com/padieer/asesoriapp/ui/theme/Theme.kt
+++ app/src/main/java/com/padieer/asesoriapp/ui/theme/Theme.kt
@@ -36,10 +36,14 @@ private val LightColorScheme = lightColorScheme(
 fun AsesoriAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
