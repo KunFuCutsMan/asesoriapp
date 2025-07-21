a2870df416f58f7483201e3580ba31a4faed6c4d
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-07-21 16:20:41 -0600

    Trnsiciones en authGraph.kt

--- app/src/main/java/com/padieer/asesoriapp/ui/nav/graph/authGraph.kt
+++ app/src/main/java/com/padieer/asesoriapp/ui/nav/graph/authGraph.kt
@@ -1,5 +1,8 @@
 package com.padieer.asesoriapp.ui.nav.graph
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -10,14 +13,45 @@ import com.padieer.asesoriapp.ui.nav.Screen
 import com.padieer.asesoriapp.ui.password.ForgotPasswordScreen
 
 fun NavGraphBuilder.authNavGraph(navController: NavController) {
-    navigation<Screen.Auth>( startDestination = Screen.Auth.InicioSesionScreen ) {
-        composable<Screen.Auth.InicioSesionScreen> {
+    val tween_time = 1000;
+    navigation<Screen.Auth>(
+        startDestination = Screen.Auth.InicioSesionScreen,
+        enterTransition = { fadeIn(tween(tween_time)) }
+    ) {
+        composable<Screen.Auth.InicioSesionScreen>(
+            enterTransition = { slideIntoContainer (
+                AnimatedContentTransitionScope.SlideDirection.Left,
+                tween(tween_time)
+            ) },
+            exitTransition = { slideOutOfContainer (
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(tween_time)
+            ) }
+        ) {
             InicioSesionScreen(navController)
         }
-        composable<Screen.Auth.CreaCuentaScreen> {
+        composable<Screen.Auth.CreaCuentaScreen>(
+            enterTransition = { slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(tween_time)
+            ) },
+            exitTransition = { slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(tween_time)
+            ) }
+        ) {
             CreaCuentaScreen(navController)
         }
-        composable<Screen.Auth.ForgotPasswordScreen> {
+        composable<Screen.Auth.ForgotPasswordScreen>(
+            enterTransition = { slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(tween_time)
+            ) },
+            exitTransition = { slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(tween_time)
+            ) }
+        ) {
             ForgotPasswordScreen(navController)
         }
     }
