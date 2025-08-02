d1bd3f7749980e31564afc05cf0214ae4f512e75
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-08-02 14:58:22 -0600

    GetLoggedInUserDataUseCase.kt

---
+++ app/src/main/java/com/padieer/asesoriapp/domain/getters/GetLoggedInUserDataUseCase.kt
@@ -0,0 +1,42 @@
+package com.padieer.asesoriapp.domain.getters
+
+import com.padieer.asesoriapp.data.carrera.CarreraRepository
+import com.padieer.asesoriapp.data.token.LoginRepository
+import com.padieer.asesoriapp.domain.error.DataError
+import com.padieer.asesoriapp.domain.error.Result
+import com.padieer.asesoriapp.domain.model.AdminModel
+import com.padieer.asesoriapp.domain.model.AsesorModel
+import com.padieer.asesoriapp.domain.model.CarreraModel
+import com.padieer.asesoriapp.domain.model.EstudianteModel
+
+class GetLoggedInUserDataUseCase(
+    private val loginRepository: LoginRepository,
+    private val carreraRepository: CarreraRepository,
+) {
+
+    data class EstudianteData(
+        val estudiante: EstudianteModel,
+        val carrera: CarreraModel,
+        val asesor: AsesorModel?,
+        val admin: AdminModel?
+    )
+
+    suspend operator fun invoke(): Result<EstudianteData, DataError> {
+        val estudianteRes = loginRepository.getLoggedInUser()
+        if (estudianteRes is Result.Error) return Result.Error(estudianteRes.error)
+
+        val estudiante = (estudianteRes as Result.Success).data
+
+        val carreraRes = carreraRepository.getCarreraByID(estudiante.id)
+        if (carreraRes is Result.Error) return Result.Error(carreraRes.error)
+
+        val carrera = (carreraRes as Result.Success).data
+
+        return Result.Success(EstudianteData(
+            estudiante = estudiante,
+            carrera = carrera,
+            asesor = estudiante.asesor,
+            admin = estudiante.asesor?.admin,
+        ))
+    }
+}
