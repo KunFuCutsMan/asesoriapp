1fd5c4cbf207881ae9d2528ee2655332242ee434
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-08-15 21:03:30 -0600

    Progreso en la tarjeta

--- app/src/main/java/com/padieer/asesoriapp/ui/asesoria/TarjetaAsesoria.kt
+++ app/src/main/java/com/padieer/asesoriapp/ui/asesoria/TarjetaAsesoria.kt
@@ -1,27 +1,42 @@
 package com.padieer.asesoriapp.ui.asesoria
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Ease
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.padieer.asesoriapp.ui.theme.AsesoriAppTheme
+import com.padieer.asesoriapp.ui.theme.isDarkTheme
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
@@ -57,119 +72,140 @@ fun TarjetaAsesoria(
     val horaInicioFormateada = timeFormatter.format(asesoria.horaInicio)
     val horaFinalFormateada = timeFormatter.format(asesoria.horaFinal)
 
-    Card(
+    OutlinedCard(
         modifier = modifier
             .fillMaxWidth()
-            .padding(8.dp),
-        shape = RoundedCornerShape(12.dp),
-        colors = CardDefaults.cardColors(containerColor = Color.White),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            .animateContentSize()
     ) {
-        Column(modifier = Modifier.padding(12.dp)) {
-
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .animateContentSize(animationSpec = tween(easing = Ease, durationMillis = 500)),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            ),
+        ) {
             // Tarjeta Asesoria
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
             ) {
-                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                Column(
+                    modifier = Modifier.weight(2f),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)) {
+
                     Text(
                         text = asesoria.asignatura.nombre,
                         style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
                     )
+                    Spacer(Modifier.height(16.dp))
+
                     Text(
                         text = asesoria.estado,
                         style = MaterialTheme.typography.bodyMedium,
-                        color = Color.Gray
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
-                    Box(
-                        modifier = Modifier
-                            .height(6.dp)
-                            .fillMaxWidth(0.4f)
-                            .clip(CircleShape)
-                            // Esto lo que hace, es que si el estado de la asesoria es Activa, se cambia el color a verde.
-                            .background(
-                                if (asesoria.estado.equals("Activa", ignoreCase = true)) Color(0xFF4CAF50)
-                                else Color.Gray
-                            )
+
+                    MarcasProgresos(
+                        modifier = Modifier.height(4.dp)
                     )
                 }
 
-                Column(horizontalAlignment = Alignment.End) {
+                Column(
+                    modifier = Modifier.weight(1f),
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.Bottom
+                )
+                {
                     Text(
                         text = fechaFormateada,
                         fontSize = 14.sp,
-                        color = Color.DarkGray
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
                     Text(
                         text = "$horaInicioFormateada - $horaFinalFormateada",
                         fontSize = 14.sp,
-                        color = Color.DarkGray
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
                     IconButton(onClick = { expanded = !expanded }) {
                         Icon(
                             imageVector = Icons.Default.ArrowDropDown,
                             contentDescription = "Mostrar más",
                             modifier = Modifier.rotate(rotation),
-                            tint = Color.Black
                         )
                     }
                 }
             }
 
-            // Parte desplegable
+        }
+
+        // Parte desplegable
+        if (expanded) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 8.dp)
-                    .clip(RoundedCornerShape(12.dp))
-                    .background(Color(0xFFF0F0F0))
-                    .animateContentSize()
-                    .padding(16.dp)
             ) {
-                if (expanded) {
-                    Divider(
-                        color = Color.LightGray,
-                        thickness = 1.dp,
-                        modifier = Modifier.padding(bottom = 8.dp)
-                    )
+                Text(
+                    text = "Asesoría dada por: ${asesoria.asesor?.nombre ?: "Sin asignar"}",
+                    style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
 
-                    Text(
-                        text = "Asesoría dada por: ${asesoria.asesor?.nombre ?: "Sin asignar"}",
-                        style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
-                        modifier = Modifier.padding(bottom = 8.dp)
-                    )
+                Text(
+                    text = "Nombre del Asesorado: ${asesoria.estudiante.nombre}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
 
-                    Text(
-                        text = "Nombre del Asesorado: ${asesoria.estudiante.nombre}",
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
+                Text(
+                    text = "Materia Correspondiente: ${asesoria.asignatura.nombre}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
 
-                    Text(
-                        text = "Materia Correspondiente: ${asesoria.asignatura.nombre}",
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
+                Text(
+                    text = "De la carrera de: ${asesoria.carrera.nombre}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
 
-                    Text(
-                        text = "De la carrera de: ${asesoria.carrera.nombre}",
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
-
-                    Text(
-                        text = "Estado de la asesoría: ${asesoria.estado}",
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
+                Text(
+                    text = "Estado de la asesoría: ${asesoria.estado}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
+
         }
+
+    }
+}
+
+@Composable
+fun MarcasProgresos(modifier: Modifier = Modifier) {
+    val activeColor = if (isDarkTheme()) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.secondary
+    val unactiveColor = if (isDarkTheme()) MaterialTheme.colorScheme.surfaceBright else MaterialTheme.colorScheme.tertiaryContainer
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+
+        Box(Modifier.fillMaxHeight().weight(4f).background(activeColor))
+
+        Spacer(Modifier.weight(1f))
+
+        Box(Modifier.fillMaxHeight().weight(4f).background(unactiveColor))
+
+        Spacer(Modifier.weight(1f))
+
+        Box(Modifier.fillMaxHeight().weight(4f).background(unactiveColor))
     }
 }
 
 // Preview para la tarjeta de asesoría
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun PreviewTarjetaAsesoria() {
     val asesoriaEjemplo = Asesoria(
@@ -182,6 +218,19 @@ fun PreviewTarjetaAsesoria() {
         estudiante = Estudiante("Luis Romero"),
         asesor = Asesor("Raúl Alberto")
     )
-    TarjetaAsesoria(asesoria = asesoriaEjemplo)
+    AsesoriAppTheme(false) {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(500.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            TarjetaAsesoria(
+                modifier = Modifier.padding(16.dp),
+                asesoria = asesoriaEjemplo
+            )
+        }
+    }
 }
 // xd
